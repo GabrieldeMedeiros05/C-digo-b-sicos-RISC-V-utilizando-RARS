cc6224fc552dd02c1f65d71c17af55196d1a138f
Gabriel <gabriel@pop-os.localdomain>
2025-06-02 02:31:25 -0300

    feat: aula 5

---
+++ Aulas/aula5.asm
@@ -0,0 +1,13 @@
+.text 			# instrucoes de assembly
+
+	li a7,5
+	ecall		# lendo um numero do teclado e salvando em a0
+	
+	addi a0,a0,-1	# o primeiro registrador e onde vai ser salvo o resultado da soma. a soma vai ser segundo registrador - numero
+			# a0 = a0-1
+	
+	li a7,1
+	ecall		# imprimir o resultado da soma/subtracao (a0)
+	
+	li a7,10
+	ecall		# return 0
