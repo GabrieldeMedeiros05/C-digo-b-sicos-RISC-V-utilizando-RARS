90ec52df5f182d74f091448f334fed30fba60947
Gabriel <gabriel@pop-os.localdomain>
2025-06-02 03:58:10 -0300

    feat: aula 11

---
+++ Aulas/aula11.1.asm
@@ -0,0 +1,23 @@
+.text 			# instrucoes de assembly
+
+	li a7,5
+	ecall		# lendo um primeiro numero do teclado e salvando em a0
+	
+	mv s0,a0	# copiando para s0 o numero em a0 (s0 = a0)
+			# estamos copiando para s0 o valor de a0 para que esse valor nao seja sobrescrito na segunda leitura
+	
+	li a7,5
+	ecall		# lendo um segunndo numero do teclado e salvando em a0
+	
+	mv s1,a0	# copiando para s1 o numero em a0 (s1 = a0)
+	
+	
+	mul a0,s0,s1 	# o primeiro registrador e onde vai ser salvo o resultado da multiplicacao
+			# segundo registrador vezes terceito registrador
+			# a0 = s0*s1
+	
+	li a7,1
+	ecall		# imprimir o resultado da multiplicacao (a0)
+	
+	li a7,10
+	ecall		# return 0
